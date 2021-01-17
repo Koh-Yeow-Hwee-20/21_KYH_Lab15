57a67ea352a58cac0d97679fc869143637f93c40
YeowHwee <yeowhwee13@gmail.com>
2021-01-17 15:28:41 +0800

    Initial commit

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip[] AudioClipArr;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            audioSource.Play();
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            audioSource.volume = 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            audioSource.volume = -0.5f;
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public GameObject jumpText;
+    public AudioClip[] AudioClipArr;
+
+    private int JumpCounter;
+    private Rigidbody rb;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpCounter++;
+
+            jumpText.GetComponent<Text>().text = "Jump: " + JumpCounter;
+
+            rb.AddForce(new Vector3(0, 250, 0));
+
+            //audioSource.Play();
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+    }
+}
